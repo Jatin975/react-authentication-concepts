f03cc17a1e859803196900e6b1f104816b10f16a
Jatin Paul <jatinpaul443@gmail.com>
2025-06-06 15:30:54 +0530

    Adding user signup with validation message

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,15 +1,32 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
+  const response = useActionData();
+  const navigation = useNavigation();
+  let isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+
+        {response?.message && <p>{response.message}</p>}
+        <ul>
+          {response?.errors &&
+            Object.values(response.errors).map((errorMsg) => (
+              <li key={errorMsg}>{errorMsg}</li>
+            ))}
+        </ul>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -22,7 +39,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,40 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function AuthAction({ request, params }) {
+  const formData = await request.formData();
+  const requestURL = new URL(request.url).searchParams;
+  const mode = requestURL.get("mode") || "login";
+
+  const authData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response("Invalid mode", { status: 422 });
+  }
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: request.method,
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw new Response("Failed to login/signup the user", { status: 500 });
+  }
+
+  return redirect("/");
+}
